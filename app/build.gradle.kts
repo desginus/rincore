6e57dd3323901d7af95b84e8c95db9fbfd332b4b
desginus <desginus@users.noreply.github.com>
2026-07-17 14:00:13 +0000

    chore: version bump 2.4.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "me.rincore.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 37
-        versionName = "2.4.12"
+        versionCode = 38
+        versionName = "2.4.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
