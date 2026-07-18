97c64257db1f792fe6f4336dd8a7471a84cd440d
desginus <desginus@users.noreply.github.com>
2026-07-18 12:56:00 +0000

    chore: version bump 2.5.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 26
         targetSdk = 37
         versionCode = 40 // 锁死, 不再递增
-        versionName = "2.5.3"
+        versionName = "2.5.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
