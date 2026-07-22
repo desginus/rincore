188e42cde0dfc8949bb974315be2f47b84136ef9
desginus <desginus@users.noreply.github.com>
2026-07-22 03:56:42 +0000

    fix: width import

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "me.rincore.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 95
+        versionCode = 96
         versionName = "2.9.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
