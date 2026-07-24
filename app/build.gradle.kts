9ec8629e0b6e1707923a3c7fb8419394882624e1
desginus <desginus@users.noreply.github.com>
2026-07-24 09:59:45 +0000

    v2.9.7 versionCode 109

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "me.rincore.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 107
+        versionCode = 109
         versionName = "2.9.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
