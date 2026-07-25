9117476896d5bc165f55d555055f7a844f5db6ba
desginus <desginus@users.noreply.github.com>
2026-07-25 09:20:34 +0000

    chore: bump versionCode=112 versionName=3.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "me.rincore.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 111
-        versionName = "2.9.8"
+        versionCode = 112
+        versionName = "3.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
