1bf94dace2239fcfae9072702e48039bdd81e0db
desginus <desginus@users.noreply.github.com>
2026-08-09 17:24:51 +0000

    fix: collectAsState import

--- app/src/main/java/me/rerere/rikkahub/ui/pages/setting/SettingToolComparePage.kt
+++ app/src/main/java/me/rerere/rikkahub/ui/pages/setting/SettingToolComparePage.kt
@@ -23,6 +23,7 @@ import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
